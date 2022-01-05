0a47a11e9afea988012b7cfde8df64400fccdd58
datnham0212 <datnham0212@gmail.com>
2022-01-05 08:48:29 +0700

    12th

--- enemy.py
+++ enemy.py
@@ -4,15 +4,15 @@ class Enemy:
 
     def __init__(self, screen, x):
         self.screen = screen
-        self.grunt1Image = pg.image.load("assets/ship1.png")
+        self.image = pg.image.load("assets/ship1.png")
         self.x = x
         self.y = 0 # <= 200
         self.velocity = 2
         self.width = 70
         self.height = 70
     
-    def drawGrunt1(self, screen):
-        screen.blit(self.grunt1Image, (self.x, self.y))
+    def draw(self, screen):
+        screen.blit(self.image, (self.x, self.y))
 
     def move(self):
         self.y += self.velocity
